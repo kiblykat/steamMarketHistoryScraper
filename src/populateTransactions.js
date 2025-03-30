e335262e73a4bdd6b4672e89c06d0b4ba9a00047
izzat <izzatfadzlon@gmail.com>
2025-03-30 15:14:20 +0800

    populate: integrate scrapeAndCleanHistory to populate database with cleaned transaction data. run this to populate backend

--- src/populateTransactions.js
+++ src/populateTransactions.js
@@ -1,19 +1,28 @@
-import axios from 'axios';
+import axios from "axios";
+import { scrapeAndCleanHistory } from "./historyScraper.js";
 
 // endpoint: http://localhost:5000/transactions/create
 
-async function populateDatabase(req, res){
-    try{
-        const response = await axios.post('http://localhost:5000/transactions/create', {
-            uid: "kiblykat",
-            steamItem: req.body.steamItem,
-            price: req.body.price,
-            type: req.body.positiveNegative,
-            quantity: req.body.date
-        });
-        console.log(response.data);
-    }catch(err){
-        console.log(err)
+async function populateDatabase(req, res) {
+  const updatedArr = await scrapeAndCleanHistory(); // [...,[ 1, 'Chroma 3 Case', 'SELL', '0.02', '2023-06-29' ]]
+  for (let arr of updatedArr) {
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/transactions/create",
+        {
+          uid: "kiblykat",
+          steamItem: arr[1], //this needs to be changed to itemName once the backend is updated
+          price: arr[3],
+          type: arr[2],
+          quantity: arr[0],
+          date: arr[4],
+        }
+      );
+      console.log(response.data);
+    } catch (err) {
+      console.log(err);
     }
+  }
 }
 
+populateDatabase();
